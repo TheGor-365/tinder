2d4b44492b687e1e81112d54286636a381a66946
Gor <7495500@gmail.com>
2021-02-23 00:05:53 +0300

    Add required permit config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_devise
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :username, :first_name, :last_name, images: [] ])
+  end
+
 end
